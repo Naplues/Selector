5ed829a803226b26c65d25fa5b739fb16e3e6dcd
naplues <naplues@163.com>
2018-07-22 09:41:37 +0800

    add GUI code Signed-off-by: naplues <naplues@163.com>

---
+++ src/nju/gzq/Main.java
@@ -0,0 +1,20 @@
+package nju.gzq;
+
+import nju.gzq.gui.Panel;
+
+import javax.swing.*;
+
+public class Main {
+    /**
+     * Program entry
+     * @param args
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Panel("Selector");
+            }
+        });
+    }
+}
